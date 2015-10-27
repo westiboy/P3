2e9eb9497efa0340112565636d85c486147ec421
Aran Gilmore <aran@arangilmore.com>
2015-10-27 19:00:53 -0400

    Change Illuminate\Http to http

--- app/Http/Controllers/P3loremController.php
+++ app/Http/Controllers/P3loremController.php
@@ -2,7 +2,7 @@
 
 namespace P3\http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\http\Request;
 use P3\http\Requests;
 use P3\http\Controllers\Controller;
 
--- resources/views/Layouts/master.blade.php
+++ resources/views/Layouts/master.blade.php
@@ -7,6 +7,8 @@
     </title>
 
     <meta charset='utf-8'>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/lumen/bootstrap.min.css" rel="stylesheet" integrity="sha256-kBMbBs0vxJSlN7T3a7hOLllj53zib5nbF7rn4NAKy04= sha512-PwtEPLjYjtwEveJNXQTih2qsQhTg4tjgIy0gruouyKN5NLD4G4jh+mMrf7cA2s5PVI71UlS8DBYr1iqrN7ivBg==" crossorigin="anonymous">
     <link href="/css/P3.css" type='text/css' rel='stylesheet'>
 
     {{-- Yield any page specific CSS files or anything else you might want in the <head> --}}
@@ -18,7 +20,7 @@
     <header>
         <img
         src='https://c1.staticflickr.com/3/2720/4119110890_8bdcd798e2.jpg'
-        style='width:500px'
+        style='width:5 00px'
         alt='P3 Logo'>
     </header>
 
@@ -31,6 +33,8 @@
         &copy; {{ date('Y') }}
     </footer>
 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha256-Sk3nkD6mLTMOF0EOpNtsIry+s1CsaqQC1rVLTAy+0yc= sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
     {{-- Yield any page specific JS files or anything else you might want at the end of the body --}}
